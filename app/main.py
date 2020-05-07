fe3aab5d6e7b0d681e02ed7512501daa7f1bb15a
Wojciech Oczkowski <wojciech.oczkowski@yougov.com>
2020-05-07 17:57:45 +0200

    Add basic input argument parsing

--- app/main.py
+++ app/main.py
@@ -1,2 +1,10 @@
+import sys
+
+no_argument_warning = 'No required argument!'
+
+
 def main():
-    pass
+    try:
+        print(sys.argv[1])
+    except IndexError:
+        print(no_argument_warning)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,37 @@
+import string
+import sys
+
+from contextlib import contextmanager, redirect_stdout
+from io import StringIO
+from random import choice
 from unittest import TestCase
 
 from app.main import main
 
 
-class TestMain(TestCase):
-    def test_can_call(self):
-        main()
+class TestCaseAssertionMixIn:
+    @contextmanager
+    def assertPrint(self) -> None:
+        with redirect_stdout(tmp_stdout := StringIO()):
+            yield (printed := [])
+        printed.extend(tmp_stdout.getvalue().strip().split('\n'))
+
+
+class TestMain(TestCase, TestCaseAssertionMixIn):
+    def setUp(self) -> None:
+        self.arg = ''.join(choice(string.ascii_letters) for _ in range(8))
+
+    def test_prints_argument_on_standard_output(self) -> None:
+        sys.argv.append(self.arg)
+        with self.assertPrint() as printed:
+            main()
+        self.assertIn(self.arg, printed)
+
+    def test_prints_warning_when_no_argument(self) -> None:
+        warning = 'No required argument!'
+        with self.assertPrint() as printed:
+            main()
+        self.assertIn(warning, printed)
+
+    def tearDown(self) -> None:
+        sys.argv = sys.argv[:1]
